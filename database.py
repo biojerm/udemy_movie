6801aecf23c8691ec2acf583943e4bb88d5c7506
Jeremy LaBarge <jeremylabarge@gmail.com>
2021-05-28 19:20:02 -0700

    add SQL queries to database.py

--- database.py
+++ database.py
@@ -1,1 +1,15 @@
 # Nothing here yet!
+
+CREATE_MOVIES_TABLE = """ CREATE TABLE IF NOT EXISTS movie (
+    title TEXT,
+    release_timestamp REAL,
+    watched INTEGER
+);"""
+
+INSERT_MOVIES = ("INSERT INTO movies (title, release_timestamp, watched)"
+  "VALUES (?, ?, 0);")
+
+SELECT_ALL_MOVIES = "SELECT * FROM movies;"
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+SELECT_WATCHED_MOVIES = "SELECT * FROM moives WHERE watched = 1;"
+
