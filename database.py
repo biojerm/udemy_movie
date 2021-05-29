6882007dfb71c7129b2369ca78f09db62b9cd8c2
Jeremy LaBarge <jeremylabarge@gmail.com>
2021-05-28 21:06:46 -0700

    flush out database functions

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
-# Nothing here yet!
+import datetime
+import sqlite3
+
 
 CREATE_MOVIES_TABLE = """ CREATE TABLE IF NOT EXISTS movie (
     title TEXT,
@@ -12,4 +14,35 @@ INSERT_MOVIES = ("INSERT INTO movies (title, release_timestamp, watched)"
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM moives WHERE watched = 1;"
+SET_MOVE_WATCHED = "UPDATE movies SET watched = 1 WHERE tile = ?;"
+
+connection = sqlite3.connect('data.db')
+
+def create_table():
+    with connection:
+        connection.execute(CREATE_MOVIES_TABLE)
+
+def add_movie(title, release_timestamp):
+    with connection:
+        connection.exectute(INSERT_MOVIES, (title, release_timestamp))
+
+def get_movies(upcomming=False):
+    with connection:
+        cursor = connection.cursor()
+        if upcomming:
+            time_stamp = datetime.datetime.today().timestamp()
+            cursor.execute(SELECT_UPCOMING_MOVIES, (time_stamp,))
+        else:
+            cursor.execute(SELECT_ALL_MOVIES)
+        return cursor.fetchall()
+
+def watch_movie(title):
+    with connection:
+        connection.execute(SET_MOVE_WATCHED, (title,))
+
+def get_watched_movies():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SELECT_WATCHED_MOVIES)
+        return cursor.fetchall()
 
